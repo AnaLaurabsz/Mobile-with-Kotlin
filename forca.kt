0c3f9fb3a9ff7eec709a2b8ed933ac1ea2e2546e
Ana Laura <analaura.b.souza1@gmail.com>
2025-08-19 19:37:07 -0300

    changes

--- forca.kt
+++ forca.kt
@@ -1,10 +1,11 @@
 fun main(args: Array<String>){
-    val massa : Double = readln().toDouble() //Atribui a variavel massa o valor digitado
-    println("Insira a Acelereação: ") //Imprime mensagem ao usuario
-    val aceleracao : Double = readln().toDouble() //Atribui a variavel aceleracao o valor digitado
+    println("Inisra a massa: ") //Imprime mensagem ao usuario
+    val massa : Double = readln().toDouble() //Atribui a variavel massa
+    println("Insira a acelereação: ") //Imprime mensagem ao usuario
+    val aceleracao : Double = readln().toDouble() //Atribui a variavel aceleracao 
     println(forca(massa, aceleracao)) //Imprime o retorno da funcao forca
  
 }
-fun forca(massa: Double, aceleracaob: Double ): Double{ //inicia a funcao forca
+fun forca(massa: Double, aceleracao: Double ): Double{ //inicia a funcao forca
     return massa * aceleracao //multiplica os valores dos argumentos passados e retorna
 }
